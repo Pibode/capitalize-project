94ace9b63db203c945dc9ecd2d8a2d81cab1d8f8
Elnur <mikailov.03@list.ru>
2025-10-03 15:33:50 +0300

    Initial commit: add capitalize function with tests

---
+++ src/capitalize.js
@@ -0,0 +1,9 @@
+const capitalize = (str) => {
+  if (str === "") {
+    return "";
+  }
+  const [firstChar, ...restChar] = str;
+  return `${firstChar.toUpperCase()}${restChar.join("")}`;
+};
+
+export default capitalize;
